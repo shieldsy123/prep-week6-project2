7cc0ce57c5114a675d42201c8765db13a0099a55
shieldsy123 <michael.t.shields@gmail.com>
2017-12-04 00:29:36 -0700

    Added jQuery functions

--- main.js
+++ main.js
@@ -11,42 +11,53 @@
 // Add your JS here.
 
 
-$(document).ready(function() {  
-    
-    
-//1. 
-   
-   
-//2.  
-    
-    
-//3. 
-   
-	    
+$(document).ready(function() {
+
+
+//1.
+$(".alertme").click(function(){
+  alert("jQuery seems easy so far.");
+});
+
+//2.
+$(".clickme").click(function(){
+  $(this).text("I was clicked.");
+});
+
+//3.
+$(".addStyle").click(function(){
+  $("button").addClass("buttonStyle");
+});
+
 //4.
- 
-   
-//5. 
+$(".addDifferentStyle").click(function(){
+  $(this).addClass("addUniqueStyle");
+});
 
-	
+//5.
+$(".removeBtn").click(function(){
+  $("button").removeClass("addUniqueStyle buttonStyle");
+});
+
 //6.
-	
-	
+$(".clickToggle").click(function(){
+  $(this).toggleClass("buttonStyle");
+});
+
 //7.
+$(".squareBtn").click(function(){
+  $(".square").hide();
+});
 
-	
-	
 //8.
-
-
+$(".circle").click(function(){
+  $(this).hide();
+});
 
 //9.
+$(".toggleRedSquare").click(function(){
+  $(".redSquare").toggle(1500);
+});
 
 
-
-});  // Close: $(document).ready(function() { 
-	
-	
-
-	
-	
+});  // Close: $(document).ready(function() {
